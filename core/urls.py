83c916cf5e2901f01d8ad2cb58dc9b1b1c346353
Fabio Oliveira <fabio20rj@gmail.com>
2020-04-17 19:25:07 -0300

    added view project detail

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from core import views as view
 
 urlpatterns = [
-    path("", view.home, name="home")
+    path("", view.home, name="home"),
+    path("project/<int:pk>/", view.project_detail, name="project_detail")
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from core.models import Project
 
 def home(request):
-    context = {
+    projects = Project.objects.all()
+    context = {'projects': projects}
+    return render(request, "blog/index.html", context)
 
-    }
-    return render(request, 'blog/index.html', context)
+def project_detail(request, pk):
+    project = Project.objects.get(pk=pk)
+    context = {'project': project}
+    return render(request, "blog/project_detail.html", context)
